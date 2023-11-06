b51ae34c7da735c7e79cd20db2ce6736331f91af
Valentine Ofoegbu <ofoegbuv@gmail.com>
2023-11-06 23:28:07 +0100

    generated Strings

---
+++ lib/generated/intl/messages_en.dart
@@ -0,0 +1,68 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a en locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'en';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "betterThanYesterday":
+            MessageLookupByLibrary.simpleMessage("{0} better than yesterday"),
+        "cryRecords": MessageLookupByLibrary.simpleMessage("Cry Records"),
+        "cumulativeCryDuration":
+            MessageLookupByLibrary.simpleMessage("Cumulative \nCry Duration"),
+        "dailyChallenge":
+            MessageLookupByLibrary.simpleMessage("Daily Challenge"),
+        "getStarted": MessageLookupByLibrary.simpleMessage("Get Started"),
+        "hourlyBreakdown":
+            MessageLookupByLibrary.simpleMessage("Hourly Breakdown"),
+        "longestCryDuration":
+            MessageLookupByLibrary.simpleMessage("Longest \nCry Duration"),
+        "next": MessageLookupByLibrary.simpleMessage("Next"),
+        "nextPredictedCry":
+            MessageLookupByLibrary.simpleMessage("Next Predicted Cry"),
+        "numberOfCryEpisode":
+            MessageLookupByLibrary.simpleMessage("Number \nof Cry Episode"),
+        "onboardHeadingFour":
+            MessageLookupByLibrary.simpleMessage("Happy Mom\n Happy Home"),
+        "onboardHeadingThree":
+            MessageLookupByLibrary.simpleMessage("Analytical Insight"),
+        "onboardHeadingTwo":
+            MessageLookupByLibrary.simpleMessage("A Cry with Meaning"),
+        "onboardingDesOne": MessageLookupByLibrary.simpleMessage(
+            "Now you can understand a lot about your new born, buckle up for an experience you will always long for."),
+        "onboardingDescFour": MessageLookupByLibrary.simpleMessage(
+            "Reduce you baby crying time whilst getting your schedule back together by planning for time of cry activity and time of quite."),
+        "onboardingDescThree": MessageLookupByLibrary.simpleMessage(
+            "Be your baby’s doctor by viewing great insight and analysis; you get to see how your baby cry activity varies in terms of duration and frequency to help you make good decisions"),
+        "onboardingDescTwo": MessageLookupByLibrary.simpleMessage(
+            "Now with great feedbacks, you can understand a lot about your new born cry patter and prepare for common cry peak period."),
+        "onboardingHeadingOne": MessageLookupByLibrary.simpleMessage(
+            "Welcome to a New Mothering Experience"),
+        "pleaseWaitWhileWeGatherBayData": MessageLookupByLibrary.simpleMessage(
+            "Please wait while we\ngather your Baby’s data..."),
+        "previous": MessageLookupByLibrary.simpleMessage("Previous"),
+        "setAlarm": MessageLookupByLibrary.simpleMessage("Set Alarm"),
+        "showMeHow": MessageLookupByLibrary.simpleMessage("Show me How"),
+        "todaysOverview":
+            MessageLookupByLibrary.simpleMessage("Today\'s Overview"),
+        "worstThanYesterday":
+            MessageLookupByLibrary.simpleMessage("{0} worst than yesterday")
+      };
+}
---
+++ lib/generated/l10n.dart
@@ -0,0 +1,318 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'intl/messages_all.dart';
+
+// **************************************************************************
+// Generator: Flutter Intl IDE plugin
+// Made by Localizely
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, lines_longer_than_80_chars
+// ignore_for_file: join_return_with_assignment, prefer_final_in_for_each
+// ignore_for_file: avoid_redundant_argument_values, avoid_escaping_inner_quotes
+
+class S {
+  S();
+
+  static S? _current;
+
+  static S get current {
+    assert(_current != null,
+        'No instance of S was loaded. Try to initialize the S delegate before accessing S.current.');
+    return _current!;
+  }
+
+  static const AppLocalizationDelegate delegate = AppLocalizationDelegate();
+
+  static Future<S> load(Locale locale) {
+    final name = (locale.countryCode?.isEmpty ?? false)
+        ? locale.languageCode
+        : locale.toString();
+    final localeName = Intl.canonicalizedLocale(name);
+    return initializeMessages(localeName).then((_) {
+      Intl.defaultLocale = localeName;
+      final instance = S();
+      S._current = instance;
+
+      return instance;
+    });
+  }
+
+  static S of(BuildContext context) {
+    final instance = S.maybeOf(context);
+    assert(instance != null,
+        'No instance of S present in the widget tree. Did you add S.delegate in localizationsDelegates?');
+    return instance!;
+  }
+
+  static S? maybeOf(BuildContext context) {
+    return Localizations.of<S>(context, S);
+  }
+
+  /// `Welcome to a New Mothering Experience`
+  String get onboardingHeadingOne {
+    return Intl.message(
+      'Welcome to a New Mothering Experience',
+      name: 'onboardingHeadingOne',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Now you can understand a lot about your new born, buckle up for an experience you will always long for.`
+  String get onboardingDesOne {
+    return Intl.message(
+      'Now you can understand a lot about your new born, buckle up for an experience you will always long for.',
+      name: 'onboardingDesOne',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `A Cry with Meaning`
+  String get onboardHeadingTwo {
+    return Intl.message(
+      'A Cry with Meaning',
+      name: 'onboardHeadingTwo',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Now with great feedbacks, you can understand a lot about your new born cry patter and prepare for common cry peak period.`
+  String get onboardingDescTwo {
+    return Intl.message(
+      'Now with great feedbacks, you can understand a lot about your new born cry patter and prepare for common cry peak period.',
+      name: 'onboardingDescTwo',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Analytical Insight`
+  String get onboardHeadingThree {
+    return Intl.message(
+      'Analytical Insight',
+      name: 'onboardHeadingThree',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Be your baby’s doctor by viewing great insight and analysis; you get to see how your baby cry activity varies in terms of duration and frequency to help you make good decisions`
+  String get onboardingDescThree {
+    return Intl.message(
+      'Be your baby’s doctor by viewing great insight and analysis; you get to see how your baby cry activity varies in terms of duration and frequency to help you make good decisions',
+      name: 'onboardingDescThree',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Happy Mom\n Happy Home`
+  String get onboardHeadingFour {
+    return Intl.message(
+      'Happy Mom\n Happy Home',
+      name: 'onboardHeadingFour',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Reduce you baby crying time whilst getting your schedule back together by planning for time of cry activity and time of quite.`
+  String get onboardingDescFour {
+    return Intl.message(
+      'Reduce you baby crying time whilst getting your schedule back together by planning for time of cry activity and time of quite.',
+      name: 'onboardingDescFour',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Previous`
+  String get previous {
+    return Intl.message(
+      'Previous',
+      name: 'previous',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Next`
+  String get next {
+    return Intl.message(
+      'Next',
+      name: 'next',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Get Started`
+  String get getStarted {
+    return Intl.message(
+      'Get Started',
+      name: 'getStarted',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Show me How`
+  String get showMeHow {
+    return Intl.message(
+      'Show me How',
+      name: 'showMeHow',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Please wait while we\ngather your Baby’s data...`
+  String get pleaseWaitWhileWeGatherBayData {
+    return Intl.message(
+      'Please wait while we\ngather your Baby’s data...',
+      name: 'pleaseWaitWhileWeGatherBayData',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Cry Records`
+  String get cryRecords {
+    return Intl.message(
+      'Cry Records',
+      name: 'cryRecords',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Number \nof Cry Episode`
+  String get numberOfCryEpisode {
+    return Intl.message(
+      'Number \nof Cry Episode',
+      name: 'numberOfCryEpisode',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Longest \nCry Duration`
+  String get longestCryDuration {
+    return Intl.message(
+      'Longest \nCry Duration',
+      name: 'longestCryDuration',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Cumulative \nCry Duration`
+  String get cumulativeCryDuration {
+    return Intl.message(
+      'Cumulative \nCry Duration',
+      name: 'cumulativeCryDuration',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `{0} better than yesterday`
+  String get betterThanYesterday {
+    return Intl.message(
+      '{0} better than yesterday',
+      name: 'betterThanYesterday',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `{0} worst than yesterday`
+  String get worstThanYesterday {
+    return Intl.message(
+      '{0} worst than yesterday',
+      name: 'worstThanYesterday',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Hourly Breakdown`
+  String get hourlyBreakdown {
+    return Intl.message(
+      'Hourly Breakdown',
+      name: 'hourlyBreakdown',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Daily Challenge`
+  String get dailyChallenge {
+    return Intl.message(
+      'Daily Challenge',
+      name: 'dailyChallenge',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Next Predicted Cry`
+  String get nextPredictedCry {
+    return Intl.message(
+      'Next Predicted Cry',
+      name: 'nextPredictedCry',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Set Alarm`
+  String get setAlarm {
+    return Intl.message(
+      'Set Alarm',
+      name: 'setAlarm',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Today's Overview`
+  String get todaysOverview {
+    return Intl.message(
+      'Today\'s Overview',
+      name: 'todaysOverview',
+      desc: '',
+      args: [],
+    );
+  }
+}
+
+class AppLocalizationDelegate extends LocalizationsDelegate<S> {
+  const AppLocalizationDelegate();
+
+  List<Locale> get supportedLocales {
+    return const <Locale>[
+      Locale.fromSubtags(languageCode: 'en'),
+    ];
+  }
+
+  @override
+  bool isSupported(Locale locale) => _isSupported(locale);
+  @override
+  Future<S> load(Locale locale) => S.load(locale);
+  @override
+  bool shouldReload(AppLocalizationDelegate old) => false;
+
+  bool _isSupported(Locale locale) {
+    for (var supportedLocale in supportedLocales) {
+      if (supportedLocale.languageCode == locale.languageCode) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
