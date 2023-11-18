ffd04672d828bb435342b02cd296703c6eec0b2a
Valentine Ofoegbu <ofoegbuv@gmail.com>
2023-11-18 02:10:04 +0100

    - completed animations refactoring of the homepage

--- lib/ui/views/onboarding/widgets/onboard_item_view.dart
+++ lib/ui/views/onboarding/widgets/onboard_item_view.dart
@@ -13,54 +13,57 @@ class OnboardItemView extends ViewModelWidget<OnboardingViewModel> {
   Widget build(BuildContext context, OnboardingViewModel viewModel) {
     var onboardingModel = viewModel.onboardingModels[viewModel.currentPage];
 
-    return Column(
-      children: [
-        _buildCarousel(onboardingModel),
-        const SizedBox(height: 24),
-        ScaleEffect(
-          page: viewModel.currentPage,
-          notifier: viewModel.pageScrollPositionNotifier,
-          child: Text(
-            onboardingModel.title,
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-                color: AppColors.text, fontSize: 22, fontWeight: FontWeight.w700),
-          ),
-        ),
-        const SizedBox(height: 24),
-        ScaleEffect(
-          page: viewModel.currentPage,
-          notifier: viewModel.pageScrollPositionNotifier,
-          child: Text(
-            onboardingModel.desc,
-            textAlign: TextAlign.center,
-            style: const TextStyle(
-                color: Colors.black,
-                fontSize: 14,
-                height: 1.5,
-                fontWeight: FontWeight.w300),
-          ),
-        ),
-        const SizedBox(height: 24),
-
-        if(viewModel.currentPage == viewModel.onboardingModels.length - 1)
-          MaterialButton(
-          onPressed: viewModel.gotoHomeView,
-          minWidth: double.maxFinite,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-          color: AppColors.primary,
-          textColor: Colors.white,
-          elevation: 0,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 14),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          _buildCarousel(onboardingModel),
+          const SizedBox(height: 24),
+          ScaleEffect(
+            page: viewModel.currentPage,
+            notifier: viewModel.pageScrollPositionNotifier,
             child: Text(
-              S.current.getStarted,
-              style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
+              onboardingModel.title,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                  color: AppColors.text, fontSize: 22, fontWeight: FontWeight.w700),
             ),
           ),
-        )
-      ],
+          const SizedBox(height: 24),
+          ScaleEffect(
+            page: viewModel.currentPage,
+            notifier: viewModel.pageScrollPositionNotifier,
+            child: Text(
+              onboardingModel.desc,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 14,
+                  height: 1.5,
+                  fontWeight: FontWeight.w300),
+            ),
+          ),
+          const SizedBox(height: 24),
+
+          if(viewModel.currentPage == viewModel.onboardingModels.length - 1)
+            MaterialButton(
+            onPressed: viewModel.gotoHomeView,
+            minWidth: double.maxFinite,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            color: AppColors.primary,
+            textColor: Colors.white,
+            elevation: 0,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 14),
+              child: Text(
+                S.current.getStarted,
+                style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 
